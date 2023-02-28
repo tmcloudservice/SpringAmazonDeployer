031ec384142dcfc42783e53218d1891eae76521e
metodiev <miroslav.l.metodiev@gmail.com>
2023-02-28 21:01:58 +0200

    add search, download and menu endpoints

--- src/main/java/com/tws/tws/contorller/ConfigurationController.java
+++ src/main/java/com/tws/tws/contorller/ConfigurationController.java
@@ -1,8 +1,10 @@
 package com.tws.tws.contorller;
 
+import com.tws.tws.model.DocumentDTO;
+import com.tws.tws.model.MenuDTO;
+import com.tws.tws.model.SearchDTO;
 import com.tws.tws.model.TWSConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -17,4 +19,30 @@ public class ConfigurationController {
         return configuration;
     }
 
+    @GetMapping("/menu")
+    @ResponseBody
+    //TODO Put here the request from the front end
+    public MenuDTO getTWSMenu() throws IOException {
+        MenuDTO menuDTO = new MenuDTO();
+        menuDTO.setHome("Home");
+        menuDTO.setProfile("Profile");
+        menuDTO.setService("Services");
+        menuDTO.setSupport("Support");
+        menuDTO.setMore("More");
+
+        return menuDTO;
+    }
+
+    @PostMapping("/search")
+    public SearchDTO webSearch(@RequestBody  SearchDTO searchDTONew) {
+        SearchDTO searchDTO = new SearchDTO();
+        searchDTO.setSearchContent(searchDTONew.getSearchContent());
+        return searchDTO;
+    }
+
+    @PostMapping("/downloadDocs")
+    public byte[] downloadFromTheServer(@RequestBody  DocumentDTO documentId) throws IOException {
+       DocumentDTO searchDTO = new DocumentDTO();
+        return searchDTO.getDocumentByID(String.valueOf(documentId.getDocumentId()));
+    }
 }
